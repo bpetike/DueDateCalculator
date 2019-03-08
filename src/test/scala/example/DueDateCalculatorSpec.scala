1a81845cf71d173534bab213badee430db35e663
Peter Bonta <peter.b@technaura.com>
2019-03-08 11:33:47 +0100

    Add test case for checkTurnAroundTime function

--- src/test/scala/example/DueDateCalculatorSpec.scala
+++ src/test/scala/example/DueDateCalculatorSpec.scala
@@ -31,11 +31,33 @@ class DueDateCalculatorSpec extends WordSpec with Matchers {
         val calculator = new DueDateCalculator
         calculator.checkWorkHours(submitDate) shouldBe false
       }
+
+      "return false if submit date is in work hours but on a non-work day" in {
+        val submitDate = LocalDateTime.of(2019, 3, 2, 13, 5, 0)
+        val calculator = new DueDateCalculator
+        calculator.checkWorkHours(submitDate) shouldBe false
+      }
     }
 
 
-    "check if turnaround time is greater than zero" in {
+    "check if turnaround time is greater than zero" should {
+      "return true if the given hours value is greater than zero" in {
+        val turnAroundTime = 9
+        val calculator = new DueDateCalculator
+        calculator.checkTurnAroundTime(turnAroundTime) shouldBe true
+      }
 
+      "return false if the given hours value is less than zero" in {
+        val turnAroundTime = 9
+        val calculator = new DueDateCalculator
+        calculator.checkTurnAroundTime(turnAroundTime) shouldBe false
+      }
+
+      "return false if turnaround time value is zero" in {
+        val turnAroundTime = 0
+        val calculator = new DueDateCalculator
+        calculator.checkTurnAroundTime(turnAroundTime) shouldBe false
+      }
     }
 
     "calculate due date" in {
